abe12709dfa727686b8e9f9cfd5008c8b42fb669
meng1992 <762577099@qq.com>
2020-01-03 17:21:44 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/shiro/ShiroConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.shiro;
+
+import org.apache.shiro.mgt.SessionsSecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    MyRealm myRealm(){
+        return new MyRealm();
+    }
+
+    @Bean
+    SessionsSecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(myRealm());
+        return securityManager;
+    }
+
+    @Bean
+    ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition definition = new DefaultShiroFilterChainDefinition();
+        definition.addPathDefinition("/doLogin", "anon");
+        definition.addPathDefinition("/**", "authc");
+        return definition;
+    }
+}
